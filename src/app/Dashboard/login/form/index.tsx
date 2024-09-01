353058e5e99702b23a840d47550a53b9b52f2ad3
mikhaelagung <mikhaelagung3@gmail.com>
2024-09-01 23:31:37 +0700

    push

--- src/app/Dashboard/login/form/index.tsx
+++ src/app/Dashboard/login/form/index.tsx
@@ -1,15 +1,46 @@
-// @flow 
+'use client'
+
+import { ActionResult, handleSignIn } from './actions';
 import React, { FC } from 'react';
+import { useFormState } from 'react-dom';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
 
 interface FormSignInProps {
     
 }
 
+const initialFormState : ActionResult = {
+    errorTitle: null,
+    errorDesc: []
+}
+
 const FormSignIn: FC<FormSignInProps> = ({ }) => {
+
+    const [state, formAction] =  useFormState(handleSignIn, initialFormState);
+    console.log(state);
+
     return (
-        <div>
-        
+        <div className=' w-full h-screen'>
+        <div className="flex min-h-full flex-i flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900">
+                    Sign in to your account
+                </h2>
+            </div>
+            <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+            <form action={formAction} className='space-y-4'>
+                <Input  className='w-full p-2 border-2 border-gray-300 rounded-md shadow-md focus:border-blue-500 focus:shadow-lg focus:outline-none 'type="email" placeholder='email' name='email' required/>
+                <Input className='md:w-full md:max-w-sm p-2 border-2 border-gray-300 rounded-md shadow-md focus:border-blue-500 focus:shadow-lg focus:outline-none' type="password" placeholder='password' name='password' required/>
+
+                <Button className='w-full' type='submit'>
+                    Submit
+                </Button>
+            </form>
+            </div>
         </div>
+    </div>
     )
  }
 
--- src/app/Dashboard/login/page.tsx
+++ src/app/Dashboard/login/page.tsx
@@ -1,7 +1,8 @@
 import { Metadata } from 'next';
 import React, { FC } from 'react';
-import { ActionResult, handleSignIn } from './actions';
+import { ActionResult, handleSignIn } from './form/actions';
 import { useFormState } from 'react-dom';
+import FormSignIn from './form/index';
 interface SignInPageProps {
 
 }
@@ -10,36 +11,9 @@ export const metadata: Metadata = {
     title: 'Dashboard | Login'
 }
 
-const initialFormState : ActionResult = {
-    errorTitle: null,
-    errorDesc: []
-}
-
 const SignInPage: FC<SignInPageProps> = ({ }) => {
-
-    const [state, formAction] =  useFormState(handleSignIn, initialFormState);
-    console.log(state);
-
     return (
-        <div className=' w-full h-screen'>
-            <div className="flex min-h-full flex-i flex-col justify-center px-6 py-12 lg:px-8">
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <h2 className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900">
-                        Sign in to your account
-                    </h2>
-                </div>
-                <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-                <form action={handleSignIn} className='space-y-4'>
-                    <input  className='w-full p-2 border-2 border-gray-300 rounded-md shadow-md focus:border-blue-500 focus:shadow-lg focus:outline-none 'type="email" placeholder='email' name='email' required/>
-                    <input className='md:w-full md:max-w-sm p-2 border-2 border-gray-300 rounded-md shadow-md focus:border-blue-500 focus:shadow-lg focus:outline-none' type="password" placeholder='password' name='password' required/>
-
-                    <button className='w-full' type='submit'>
-                        Submit
-                    </button>
-                </form>
-                </div>
-            </div>
-        </div>
+        <FormSignIn/>
     )
 } 
 
